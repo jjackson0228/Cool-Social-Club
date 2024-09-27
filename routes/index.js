15bfeb5ac10f2a06034064b0f408f100bb49706b
James Jackson <jjackson0228@gmail.com>
2024-09-27 19:34:55 -0400

    added router.use for api routes of userRoutes and thoughtRoutes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const userRoutes = require('./api/userRoutes');
-
-//use user routes
-router.use('/api/users', userRoutes);
+const userRoutes = require('./api/userRoutes'); //added userRoutes and defined it to use ('api/userRoutes') so I can use them in the router
+const thoughtRoutes = require('./api/thoughtRoutes'); //added thoughtRoutes and defined it to use ('api/thoughtRoutes')so I can use them in the router
+//use userRoutes and thoughtRoutes
+router.use('/api/users', userRoutes); //router will use the api routes for users in a variable called userRoutes
+router.use('/api/thoughts', thoughtRoutes); //router will use the api routes for users in a variable called thoughtRoutes
 
 module.exports = router;
